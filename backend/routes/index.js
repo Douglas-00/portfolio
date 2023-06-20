d6d97593242d9d768581dd723ed1499f7aa1e03e
Douglas-00 <alvesdouglas547@gmail.com>
2023-06-20 15:23:41 -0300

    "Mangoose Conected"

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,17 +1,23 @@
 const router = require('express').Router();
 
+//Chamando o Banco de Dados
+require('../db/mongoConnection');
+
+//Chamando o arquivo portfolio 
 const portfolio = require('./portfolio');
 
-router.get('/',(req,res)=>{
+
+router.get('/', (req, res) => {
     res.json({
-        sucess:false,
-        message:'Please, do not call /api, it is for our server'
+        sucess: false,
+        message: 'Please, do not call /api, it is for our server'
     })
 });
 
-router.use('/portfolio',portfolio);
+//Rotas do Portfolio
+router.use('/portfolio', portfolio);
 
- 
+
 
 
 module.exports = router
--- server.js
+++ server.js
@@ -8,12 +8,6 @@ const app = express();
 
 app.use(bodyParser.json());
 
-console.log("Nome do banco de dados", process.env.MONGO_DB);
-console.log("Nome do usuario no banco de dados", process.env.MONGO_USER);
-
-
-
-
 app.get('/', (req, res) => {
     res.json({
         'sucess': true

6de3a4d7ac70a054d13245c9afd892c3a994c34e
Douglas-00 <alvesdouglas547@gmail.com>
2023-06-20 15:49:45 -0300

    "Atualizando Rota e Criando Model"

--- backend/routes/portfolio.js
+++ backend/routes/portfolio.js
@@ -1,22 +1,13 @@
 const router = require("express").Router();
 
 router.get('/', (req, res) => {
-    //acess db
-    const data = [{
-            id: 1,
-            name: "Douglas alves",
-            createdAt: "2023-06-20"
-        },
-        {
-            id: 2,
-            name: "Camila Franque",
-            createdAt: "2023-06-19"
-        },
-     ];
     res.json({
-        sucess: true,
-        data: data
+        success:true
     });
+})
+
+router.post('/',(req,res)=>{
+    
 })
 
 module.exports = router
